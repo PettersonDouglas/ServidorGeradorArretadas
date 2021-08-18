3c9ebdd64abf13616ec98b746f37ad912cfa123c
João Victor Lacerda de Queiroz <victorqueiroz90@outlook.com>
2021-08-17 22:44:18 -0300

    17/08/2021

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,11 +11,12 @@ public class Main {
             ServerSocket servidor = new ServerSocket(3334);//Classe responsável por esperar conexão
             System.out.println("Aguardando conexão");
             Socket cliente; //conexão entre duas máquinas
-            ConexaoDb conexao = ConexaoDb.getInstance();// conexão com o db
+            ConexaoDb conexao;
             while(true){
                 cliente = servidor.accept();//escuta a conexão
                 System.out.println("Conectado a: "+cliente.getInetAddress().getHostAddress());
                 Scanner scanner = new Scanner(cliente.getInputStream());
+                conexao = ConexaoDb.getInstance();// conexão com o db
                 String msg = scanner.nextLine();
                 if(msg.equals("") || msg.equals("sair")){
                     break;
