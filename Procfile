bb4dc8a8030a41196db2368e3248495d97073988
João Victor Lacerda de Queiroz <victorqueiroz90@outlook.com>
2021-08-19 14:21:23 -0300

    19/08/2021

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: java -jar target/ServidorGeradorDeSenha.jar
+worker: java -jar target/ServidorGeradorDeSenha.jar
--- src/main/java/ConexaoBD/ConexaoDb.java
+++ src/main/java/ConexaoBD/ConexaoDb.java
@@ -5,7 +5,6 @@ import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
 
-
 public class ConexaoDb {
     //password of db: hwgyrBaQSpvJpQO4
     //compass: mongodb+srv://dev:hwgyrBaQSpvJpQO4@cluster0.jpcti.mongodb.net/test
